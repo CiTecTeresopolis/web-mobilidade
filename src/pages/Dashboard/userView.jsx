50d19274354e8800e2ee14252a08c7b1b139078b
CiTecTeresopolis <smct.desenvolvimento@gmail.com>
2026-07-24 14:20:24 -0300

    protegendo

--- src/pages/Dashboard/userView.jsx
+++ src/pages/Dashboard/userView.jsx
@@ -14,6 +14,7 @@ import {
 import { styles } from "./styles";
 
 
+
 const API_URL = "https://api-mobilidade.vercel.app/api/auth";
 const SUPABASE_PROJECT_URL = "https://dqxlyhdmfrmucelgweko.supabase.co";
 const BUCKET_NAME = "uploads";
